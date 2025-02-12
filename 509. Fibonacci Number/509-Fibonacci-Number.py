5c5a63ef72d71125fffadf7211f0761fca478854
Gunik Luthra <65220549+gun310801@users.noreply.github.com>
2025-02-12 16:29:49 -0500

    [509] [Time Beats: 29.60%] [Memory Beats: 48.32%] - LeetPush

---
+++ 509. Fibonacci Number/509-Fibonacci-Number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <=1:
+            return n
+        last = self.fib(n-1)
+        slast = self.fib(n-2)
+        return last + slast
+        
+        
