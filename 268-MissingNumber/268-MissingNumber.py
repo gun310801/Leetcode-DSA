86166f4267452a28e73994eed1212feb9fc3adb6
Gunik Luthra <65220549+gun310801@users.noreply.github.com>
2025-02-12 17:03:32 -0500

    Create file

---
+++ 268-MissingNumber/268-MissingNumber.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        for i in range(0,len(nums)):
+            if nums[i]!= i:
+                return i
+        return len(nums)
+        
